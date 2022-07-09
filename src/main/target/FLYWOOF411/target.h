0c1e25fe10ba381af6104fc02e1aab029e77926f
trogalko <trogalko@gmail.com>
2022-07-09 11:05:28 +0700

    Update target.h
    wakata

--- src/main/target/FLYWOOF411/target.h
+++ src/main/target/FLYWOOF411/target.h
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+//wakata
 #pragma once
 
 #ifdef FLYWOOF411_V2
